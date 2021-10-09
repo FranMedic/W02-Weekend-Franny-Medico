b8783e6901afdf9efbb93bd2607890f5f6b333df
FranMedic <m.francisca.medico@gmail.com>
2021-10-09 13:05:27 +0200

    changed neighbors check with if to check neighbors with 2 function

--- build/game-of-life.js
+++ build/game-of-life.js
@@ -1,37 +1,54 @@
 const dimensionTabla = 5;
-const tabla = Array(dimensionTabla)
+const tablaTotal = Array(dimensionTabla)
   .fill(0)
   .map(() => Array(dimensionTabla).fill(0)); // filas y columnas INVESTIGAR
 
-tabla[1][0] = 1;
-tabla[1][1] = 1;
+tablaTotal[1][0] = 1;
+tablaTotal[1][1] = 1;
 
-for (let i = 0; i < dimensionTabla; i++) {
-  for (let j = 0; j < dimensionTabla; j++) {
-    console.log(i, j);
-    let contador = 0;
-    if (i === 0 && j === 0) {
-      if (tabla[0][1] === 1) contador++;
-      if (tabla[1][0] === 1) contador++;
-      if (tabla[1][1] === 1) contador++;
-      console.log(`c${contador}`);
-    } else if (i === 0 && j === tabla[j].length - 1) {
-      if (tabla[0][j.length - 2] === 1) contador++;
-      if (tabla[1][j.length - 2] === 1) contador++;
-      if (tabla[1][j.length - 1] === 1) contador++;
-      console.log(`c${contador}`);
-    } else if (i === tabla.length - 1 && j === 0) {
-      if (tabla[tabla.length - 2][0] === 1) contador++;
-      if (tabla[tabla.length - 2][1] === 1) contador++;
-      if (tabla[tabla.length - 1][1] === 1) contador++;
-      console.log(`c${contador}`);
-    } else if (i === tabla.length - 1 && j === tabla[j].length - 1) {
-      if (tabla[tabla.length - 2][j.length - 1] === 1) contador++;
-      if (tabla[tabla.length - 2][j.length - 2] === 1) contador++;
-      if (tabla[tabla.length - 1][j.length - 2] === 1) contador++;
-      console.log(`c${contador}`);
+// primero creamos ARRAY
+// segundo, leemos todas la posiciones del array
+// tercero, miramos cuantos vecinos tiene el array mirando en cada posicion sus 8 posibles vecinos
+// (si sus posiciones tienen un 1, el contador sube)
+// cuatro una vez recogidos, cuantos vecinos estan alrededor de cada posicion del array,
+
+function checkVida(tabla) {
+  for (let i = 0; i < tabla.length; i++) {
+    for (let j = 0; j < tabla[i].length; j++) {
+      // eslint-disable-next-line no-use-before-define
+      const vecinos = checkVecinos(tabla, i, j);
+      if (vecinos < 2 || vecinos > 3) {
+        tabla[i][j] = 0;
+      }
+      if (vecinos === 2 || vecinos === 3) {
+        tabla[i[j]] = 1;
+      }
     }
   }
 }
+function checkVecinos(juego, x, y) {
+  let contador = 0;
+  // VERTICAL
+  if (juego[x - 1 >= 0 && y >= 0] === 1) contador++;
 
-console.table(tabla);
+  if (juego[x + 1 >= 0 && y >= 0] === 1) contador++;
+
+  // HORIZONTAL
+  if (juego[x >= 0 && y - 1 >= 0] === 1) contador++;
+
+  if (juego[x >= 0 && y + 1 >= 0] === 1) contador++;
+
+  // DIAGONALes  ARRIBA
+  if (juego[x - 1 >= 0 && y - 1 >= 0] === 1) contador++;
+
+  if (juego[x - 1 >= 0 && y + 1 >= 0] === 1) contador++;
+
+  // DIAGONALES ABAJO
+  if (juego[x + 1 >= 0 && y - 1 >= 0] === 1) contador++;
+
+  if (juego[x + 1 >= 0 && y + 1 >= 0] === 1) contador++;
+
+  return contador;
+}
+
+console.table(checkVida(tablaTotal));
